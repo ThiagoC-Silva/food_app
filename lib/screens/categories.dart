8e80bf3849046b4807ea341256d78d6f9777b3aa
ThiagoC-Silva <thi89248@gmail.com>
2024-10-26 19:46:54 -0300

    Implement CategoryGridItem widget with InkWell for interaction and gradient background

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/data/dummy_data.dart';
+import 'package:food_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget{
   const CategoriesScreen({super.key});
